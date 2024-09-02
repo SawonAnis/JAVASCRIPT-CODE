f101859539e22009f145a597351423165ba55072
Sawon <141425167+SawonAnis@users.noreply.github.com>
2024-09-02 19:07:56 +0600

    Add files via upload

---
+++ script1.js
@@ -0,0 +1,23 @@
+console.log("sawon");
+
+let firstName = "Bob";
+console.log(firstName);
+
+let javascriptFun = true;
+console.log(javascriptFun);
+
+console.log(typeof javascriptFun);
+
+let age = 24;
+const birthYear = 2000;
+
+const anikMass = 80;
+const shihabMass = 75;
+
+const H_anik = 1.95;
+const H_shihab = 1.76;
+
+const anik_BMI = anikMass / H_anik ** 2;
+const shihab_BMI = shihabMass / H_shihab ** 2;
+
+console.log(anik_BMI, shihab_BMI);
